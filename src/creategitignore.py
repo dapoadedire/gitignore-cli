a62ff23b88d702abd603a5fa0888c0bc4271d7d8
bovage <fayemibolu2@gmail.com>
2022-11-30 05:43:17 -1200

    feat: add suggestion of language(s) if language typo

--- src/creategitignore.py
+++ src/creategitignore.py
@@ -3,6 +3,7 @@ import argparse
 
 from colorama import Fore
 from colorama import Style
+import utils
 
 # print(f"{Fore.RED}Error{Style.RESET_ALL} This is {Fore.GREEN}{name}{Style.RESET_ALL}!")
 def create_gitignore(language):
@@ -16,8 +17,13 @@ def create_gitignore(language):
             f.write(text)
         print(f"{Fore.GREEN}Success!:  .gitignore file created{Style.RESET_ALL}")
 
-    except :
-        print(f"{Fore.RED}Error!: try again.{Style.RESET_ALL} \n.gitignore file for {Fore.RED}{language}{Style.RESET_ALL} not found. \nYou are not connected to the internet or the language you entered is not supported yet. \nCheck out {Fore.GREEN}https://github.com/github/gitignore{Style.RESET_ALL} for list of available languages.")
+    except:
+        if utils.check_typo(language):
+            print(f"Did you mean:")
+            for language in utils.get_most_similar_languages(language):
+                print(f"\t{language}")
+        else:
+            print(f"{Fore.RED}Error!: try again.{Style.RESET_ALL} \n.gitignore file for {Fore.RED}{language}{Style.RESET_ALL} not found. \nYou are not connected to the internet or the language you entered is not supported yet. \nCheck out {Fore.GREEN}https://github.com/github/gitignore{Style.RESET_ALL} for list of available languages.")
             
 
 
